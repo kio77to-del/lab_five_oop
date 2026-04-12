6bc2f0d6a2f4b715e9f7d2f323096dabba0ab2b3
kio77to-del <kio77to@gmail.com>
2026-04-12 22:42:43 +0500

    добавил демонстрацию работы через указатель на базовый класс

--- main.cpp
+++ main.cpp
@@ -5,25 +5,51 @@
 
 using namespace std;
 
-int main() {
-    cout << "Lab 5 started" << endl;
+void demoSimpleObjects() {
+    cout << "\n=== Обычные объекты ===" << endl;
 
-    cout << "\n--- Shape object ---" << endl;
     Shape s1;
     s1.info();
     s1.draw();
 
-    cout << "\n--- Circle object ---" << endl;
+    cout << endl;
+
     Circle c1;
     c1.info();
     c1.draw();
     c1.onlyCircleMethod();
 
-    cout << "\n--- Rectangle object ---" << endl;
+    cout << endl;
+
     Rectangle r1;
     r1.info();
     r1.draw();
     r1.onlyRectangleMethod();
+}
+
+void demoBasePointers() {
+    cout << "\n=== Указатели на базовый класс ===" << endl;
+
+    Shape* p1 = new Shape();
+    Shape* p2 = new Circle(5.0);
+    Shape* p3 = new Rectangle(4.0, 6.0);
+
+    cout << "\nВызов draw() через Shape*:" << endl;
+    p1->draw();
+    p2->draw();
+    p3->draw();
+
+    cout << "\nУдаление объектов через Shape*:" << endl;
+    delete p1;
+    delete p2;
+    delete p3;
+}
+
+int main() {
+    cout << "Lab 5 started" << endl;
+
+    demoSimpleObjects();
+    demoBasePointers();
 
     cout << "\nProgram finished" << endl;
     return 0;
