27518509f81311a469f2a7cd861a581c8a10e251
kio77to-del <kio77to@gmail.com>
2026-04-16 22:22:14 +0500

    добавил сравнение static_cast и dynamic_cast

--- main.cpp
+++ main.cpp
@@ -1,43 +1,38 @@
 #include <iostream>
 #include <memory>
+
 #include "Shape.h"
 #include "Circle.h"
 #include "Rectangle.h"
 
 using namespace std;
 
-void printSeparator() {
-    cout << "\n------------------------------------------\n" << endl;
-}
-
 void demoSimpleObjects() {
     cout << "\n=== Обычные объекты ===" << endl;
 
-    Shape s1;
-    s1.info();
-    s1.draw();
-
+    Shape s;
+    s.info();
+    s.draw();
     cout << endl;
 
-    Circle c1;
-    c1.info();
-    c1.draw();
-    c1.onlyCircleMethod();
-
+    Circle c;
+    c.info();
+    c.draw();
+    c.onlyCircleMethod();
     cout << endl;
 
-    Rectangle r1;
-    r1.info();
-    r1.draw();
-    r1.onlyRectangleMethod();
+    Rectangle r;
+    r.info();
+    r.draw();
+    r.onlyRectangleMethod();
 }
 
 void demoBasePointers() {
     cout << "\n=== Указатели на базовый класс ===" << endl;
 
     Shape* p1 = new Shape();
-    Shape* p2 = new Circle(5.0);
-    Shape* p3 = new Rectangle(4.0, 6.0);
+    Shape* p2 = new Circle(5);
+    Shape* p3 = new Rectangle(4, 6);
 
     cout << "\nВызов draw() через Shape*:" << endl;
     p1->draw();
@@ -53,7 +48,7 @@ void demoBasePointers() {
 void demoCopyConstructors() {
     cout << "\n=== Конструкторы копирования ===" << endl;
 
-    Circle c1(8.0);
+    Circle c1(8);
     cout << "Создаем копию Circle:" << endl;
     Circle c2 = c1;
 
@@ -65,9 +60,9 @@ void demoCopyConstructors() {
 
     cout << endl;
 
-    Rectangle r1(2.0, 9.0);
+    Rectangle r1(2, 9);
     cout << "Создаем копию Rectangle:" << endl;
-    Rectangle r2(r1);
+    Rectangle r2 = r1;
 
     cout << "Исходный объект Rectangle:" << endl;
     r1.draw();
@@ -80,8 +75,8 @@ void demoVirtualAndNonVirtual() {
     cout << "\n=== Невиртуальный и виртуальный вызов ===" << endl;
 
     Shape* p1 = new Shape();
-    Shape* p2 = new Circle(10.0);
-    Shape* p3 = new Rectangle(7.0, 3.0);
+    Shape* p2 = new Circle(10);
+    Shape* p3 = new Rectangle(7, 3);
 
     cout << "\nПрямой вызов simpleAction() через Shape*:" << endl;
     p1->simpleAction();
@@ -108,29 +103,29 @@ void demoVirtualAndNonVirtual() {
 void demoDynamicCast() {
     cout << "\n=== dynamic_cast ===" << endl;
 
-    Shape* p1 = new Circle(12.0);
-    Shape* p2 = new Rectangle(5.0, 8.0);
+    Shape* p1 = new Circle(12);
+    Shape* p2 = new Rectangle(5, 8);
     Shape* p3 = new Shape();
 
     Circle* c1 = dynamic_cast<Circle*>(p1);
     Circle* c2 = dynamic_cast<Circle*>(p2);
     Circle* c3 = dynamic_cast<Circle*>(p3);
 
-    if (c1 != nullptr) {
+    if (c1) {
         cout << "p1 успешно приведен к Circle*" << endl;
         c1->onlyCircleMethod();
     } else {
         cout << "p1 не является Circle" << endl;
     }
 
-    if (c2 != nullptr) {
+    if (c2) {
         cout << "p2 успешно приведен к Circle*" << endl;
         c2->onlyCircleMethod();
     } else {
         cout << "p2 не является Circle" << endl;
     }
 
-    if (c3 != nullptr) {
+    if (c3) {
         cout << "p3 успешно приведен к Circle*" << endl;
         c3->onlyCircleMethod();
     } else {
@@ -180,6 +175,71 @@ void demoManualTypeCheck() {
     }
 }
 
+void demoCastingComparison() {
+    cout << "\n=== Сравнение static_cast и dynamic_cast ===" << endl;
+
+    Shape* p1 = new Circle(9);
+    Shape* p2 = new Rectangle(4, 5);
+    Shape* p3 = new Shape();
+
+    cout << "\nПробуем ручную проверку через isA + static_cast:" << endl;
+
+    if (p1->isA("Circle")) {
+        cout << "p1 действительно Circle, можно делать static_cast" << endl;
+        Circle* c1 = static_cast<Circle*>(p1);
+        c1->onlyCircleMethod();
+    } else {
+        cout << "p1 не Circle" << endl;
+    }
+
+    if (p2->isA("Circle")) {
+        cout << "p2 действительно Circle, можно делать static_cast" << endl;
+        Circle* c2 = static_cast<Circle*>(p2);
+        c2->onlyCircleMethod();
+    } else {
+        cout << "p2 не Circle, делать static_cast опасно" << endl;
+    }
+
+    if (p3->isA("Circle")) {
+        cout << "p3 действительно Circle, можно делать static_cast" << endl;
+        Circle* c3 = static_cast<Circle*>(p3);
+        c3->onlyCircleMethod();
+    } else {
+        cout << "p3 не Circle, делать static_cast опасно" << endl;
+    }
+
+    cout << "\nТеперь пробуем dynamic_cast:" << endl;
+
+    Circle* dc1 = dynamic_cast<Circle*>(p1);
+    Circle* dc2 = dynamic_cast<Circle*>(p2);
+    Circle* dc3 = dynamic_cast<Circle*>(p3);
+
+    if (dc1) {
+        cout << "dynamic_cast для p1 прошел успешно" << endl;
+        dc1->onlyCircleMethod();
+    } else {
+        cout << "dynamic_cast: p1 не Circle" << endl;
+    }
+
+    if (dc2) {
+        cout << "dynamic_cast для p2 прошел успешно" << endl;
+        dc2->onlyCircleMethod();
+    } else {
+        cout << "dynamic_cast: p2 не Circle" << endl;
+    }
+
+    if (dc3) {
+        cout << "dynamic_cast для p3 прошел успешно" << endl;
+        dc3->onlyCircleMethod();
+    } else {
+        cout << "dynamic_cast: p3 не Circle" << endl;
+    }
+
+    delete p1;
+    delete p2;
+    delete p3;
+}
+
 void takeByValue(Circle c) {
     cout << "Функция takeByValue() начала работу" << endl;
     c.draw();
@@ -194,15 +254,13 @@ void takeByReference(const Circle& c) {
 
 void takeByPointer(const Circle* c) {
     cout << "Функция takeByPointer() начала работу" << endl;
-    if (c != nullptr) {
-        c->draw();
-    }
+    c->draw();
     cout << "Функция takeByPointer() завершена" << endl;
 }
 
 Circle createCircle() {
     cout << "Функция createCircle() начала работу" << endl;
-    Circle temp(15.0);
+    Circle temp(15);
     cout << "Функция createCircle() возвращает объект" << endl;
     return temp;
 }
@@ -217,37 +275,37 @@ Shape createShapeObject() {
 void demoFunctionLifecycle() {
     cout << "\n=== Передача и возврат объектов из функций ===" << endl;
 
-    Circle c1(20.0);
+    Circle c(20);
 
-    printSeparator();
+    cout << "\n------------------------------------------\n" << endl;
     cout << "Передача по значению:" << endl;
-    takeByValue(c1);
+    takeByValue(c);
 
-    printSeparator();
+    cout << "\n------------------------------------------\n" << endl;
     cout << "Передача по ссылке:" << endl;
-    takeByReference(c1);
+    takeByReference(c);
 
-    printSeparator();
+    cout << "\n------------------------------------------\n" << endl;
     cout << "Передача по указателю:" << endl;
-    takeByPointer(&c1);
+    takeByPointer(&c);
 
-    printSeparator();
+    cout << "\n------------------------------------------\n" << endl;
     cout << "Возврат Circle из функции:" << endl;
-    Circle c2 = createCircle();
+    Circle newCircle = createCircle();
     cout << "Объект, полученный из createCircle():" << endl;
-    c2.draw();
+    newCircle.draw();
 
-    printSeparator();
+    cout << "\n------------------------------------------\n" << endl;
     cout << "Возврат Shape из функции:" << endl;
-    Shape s1 = createShapeObject();
+    Shape newShape = createShapeObject();
     cout << "Объект, полученный из createShapeObject():" << endl;
-    s1.draw();
+    newShape.draw();
 }
 
 void demoUniquePtr() {
     cout << "\n=== unique_ptr ===" << endl;
 
-    unique_ptr<Shape> ptr1 = make_unique<Circle>(25.0);
+    unique_ptr<Shape> ptr1 = make_unique<Circle>(25);
 
     cout << "Объект внутри unique_ptr:" << endl;
     ptr1->draw();
@@ -255,7 +313,7 @@ void demoUniquePtr() {
     cout << "Передаем владение другому unique_ptr" << endl;
     unique_ptr<Shape> ptr2 = move(ptr1);
 
-    if (ptr1 == nullptr) {
+    if (!ptr1) {
         cout << "ptr1 теперь пустой после move()" << endl;
     }
 
@@ -268,7 +326,7 @@ void demoUniquePtr() {
 void demoSharedPtr() {
     cout << "\n=== shared_ptr ===" << endl;
 
-    shared_ptr<Shape> ptr1 = make_shared<Rectangle>(11.0, 4.0);
+    shared_ptr<Shape> ptr1 = make_shared<Rectangle>(11, 4);
     cout << "После создания ptr1, use_count = " << ptr1.use_count() << endl;
 
     {
@@ -292,7 +350,9 @@ void demoSmartPointers() {
     cout << "\n=== Умные указатели ===" << endl;
 
     demoUniquePtr();
-    printSeparator();
+
+    cout << "\n------------------------------------------\n" << endl;
+
     demoSharedPtr();
 }
 
@@ -305,6 +365,7 @@ int main() {
     demoVirtualAndNonVirtual();
     demoDynamicCast();
     demoManualTypeCheck();
+    demoCastingComparison();
     demoFunctionLifecycle();
     demoSmartPointers();
 
